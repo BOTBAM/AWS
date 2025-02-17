c738be3a665e6211537c17ff0478e9997e904ecf
C0D3GH0ST <167582022+C0D3GH0ST@users.noreply.github.com>
2025-02-16 20:17:50 -0500

    Database initialization
    Code to initialize database to store our users data

--- app.py
+++ app.py
@@ -9,3 +9,19 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
+
+
+# Initialize database
+def init_db():
+    with sqlite3.connect("database.db") as conn:
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            email TEXT NOT NULL,
+            address TEXT NOT NULL)''')
+        conn.commit()
+init_db()
